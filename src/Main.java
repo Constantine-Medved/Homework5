41fb5e6fa9481bee3a92b27fc27d1dfadc43722c
Constantine-Medved <medved.k@outlook.com>
2023-03-10 12:14:02 +0300

    Tasks 1,2 are done

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,40 @@ public class Main {
     }
     public static void task1() {
         System.out.println("Задача 1.");
+        char clientOS = '1';
+        switch (clientOS) {
+            case '1':
+                System.out.println("Установите версию приложения для Android по ссылке.");
+                break;
+            case '0':
+                System.out.println("Установите версию приложения для iOS по ссылке.");
+                break;
+            default:
+                System.out.println("Сожалеем, Ваша опереционная система не поддерживается. Подерживаемые ОС: iOS и Android.");
+        }
     }
     public static void task2() {
         System.out.println("Задача 2.");
+        char clientOS = '0';
+        int clientDeviceYear = 2014;
+        switch (clientOS) {
+            case '1':
+                if (clientDeviceYear >= 2015) {
+                    System.out.println("Установите версию приложения для Android по ссылке.");
+                } else {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+                }
+                break;
+            case '0':
+                if (clientDeviceYear >= 2015) {
+                    System.out.println("Установите версию приложения для iOS по ссылке.");
+                } else {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+                }
+                break;
+            default:
+                System.out.println("Сожалеем, Ваша опереционная система не поддерживается. Подерживаемые ОС: iOS и Android.");
+        }
     }
     public static void task3() {
         System.out.println("Задача 3.");
